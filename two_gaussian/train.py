bba4aab8f765ba4b8cf0c5aab82df5fd42261531
Youngnam Kim <hoholam0112@gmail.com>
2020-05-18 16:44:17 +0900

    extend to multivariate gaussian case

--- two_gaussian/train.py
+++ two_gaussian/train.py
@@ -6,16 +6,20 @@ import torch.nn as nn
 import torch.optim as optim
 import progressbar
 import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
 from mine import MINE
+from torchutils.logging import hyperparameter_string
 
 class StatisticsNetwork(nn.Module):
-    def __init__(self, hidden_units):
+    def __init__(self, input_dim1, input_dim2, hidden_units):
         super().__init__()
+        self.input_dim1 = input_dim1
+        self.input_dim2 = input_dim2
         self.hidden_units = hidden_units
 
-        self.activation = nn.ReLU()
-        self.layer1 = nn.Linear(2, hidden_units)
+        self.activation = nn.SELU()
+        self.layer1 = nn.Linear(input_dim1 + input_dim2, hidden_units)
         self.layer2 = nn.Linear(hidden_units, hidden_units)
         self.head = nn.Linear(hidden_units, 1)
 
@@ -27,53 +31,113 @@ class StatisticsNetwork(nn.Module):
         output = self.head(h)
         return output
 
+def get_covariance_matrix(dim, correlation):
+    """ Compute mutual information analytically  """
+    cov = np.zeros([2*dim, 2*dim])
+    for i in range(dim):
+        cov[i, i + dim] = correlation
+        cov[i + dim, i] = correlation
+        cov[i, i] = 1
+        cov[i + dim, i + dim] = 1
+    return cov
+
+def compute_true_mutual_information(dim, correlation):
+    """ Compute mutual information analytically  """
+    cov = get_covariance_matrix(dim, correlation)
+    det_cov = np.linalg.det(cov)
+    if det_cov == 0:
+        raise ValueError('determinant of covariance matrix is 0.')
+    return -0.5 * np.log(det_cov)
+
+def get_samples(dim, correlation, num_samples):
+    data = np.random.multivariate_normal(
+                mean=np.zeros(2*dim),
+                cov=get_covariance_matrix(dim, correlation),
+                size=num_samples)
+    return data.astype(np.float32)
+
 def run(args):
     """ train model """
     # Define argument 
     device = torch.device('cuda:{}'.format(args.gpu)
                     if torch.cuda.is_available() else 'cpu')
 
-    hidden_units = 10
-    var_x, var_z = 1.0, 1.0
-    batch_size = 256
-    total_steps = 5000
+    hidden_units = 512
+    dim = 20
+    correlation = 0.9
+    #sigma_x, sigma_z = 1.0, 0.09
+    batch_size = 10000
+    total_steps = 50000
+    lr = 1e-3
+    weight_decay = 0.0
+    ema_decay = 0.999
+    criterion = 'mine-d'
+
+    # Define tensorboard summary writer
+    save_dir = hyperparameter_string(hidden_units=hidden_units,
+                                     dim=dim,
+                                     correlation=correlation,
+                                     batch_size=batch_size,
+                                     total_steps=total_steps,
+                                     lr=lr,
+                                     weight_decay=weight_decay,
+                                     ema_decay=ema_decay,
+                                     criterion=criterion)
+    writer = SummaryWriter('./train_logs/two_gaussian/{}'.format(save_dir))
 
     # Build modules
-    statistics_network = StatisticsNetwork(hidden_units)
+    statistics_network = StatisticsNetwork(
+            dim, dim, hidden_units)
     statistics_network.to(device)
 
     optimizer = optim.Adam(statistics_network.parameters(),
-                          lr=0.001)
+                           lr=lr)
 
     mine_object = MINE(statistics_network,
-                       ema_decay=0.999)
+                       criterion,
+                       ema_decay)
 
     # Define data loader
-    analystic_mi = np.log(1 + var_x/var_z) / 2
+    #true_mi = np.log(1 + sigma_x**2/sigma_z**2) / 2
+    true_mi = compute_true_mutual_information(
+            dim, correlation)
     eMIs = np.zeros(total_steps, dtype=np.float32)
+
 
     statistics_network.train()
     pbar = progressbar.ProgressBar()
     for i in pbar(range(total_steps)):
-        x = torch.randn(batch_size, 1) * np.sqrt(var_x)
-        z = torch.randn(batch_size, 1) * np.sqrt(var_z)
-        y = x + z
+        #x = torch.randn(batch_size, dim) * sigma_x
+        #z = torch.randn(batch_size, dim) * sigma_z
+        #y = x + z
 
+        #x = x.to(device)
+        #y = y.to(device)
+        #z = z.to(device)
+
+        batch = get_samples(dim, correlation, batch_size)
+        x = torch.from_numpy(batch[:, :dim])
+        y = torch.from_numpy(batch[:, dim:])
         x = x.to(device)
         y = y.to(device)
-        z = z.to(device)
+
         eMI, loss = mine_object.estimate_on_batch(x, y)
 
-        eMIs[i] = mine_object.eMI_ema
+        #eMIs[i] = mine_object.eMI_ema
+        eMIs[i] = eMI
 
         optimizer.zero_grad()
-        loss = -eMI
+        #loss = -eMI
         loss.backward()
         optimizer.step()
 
+        writer.add_scalar('eMI', eMI, i)
+        writer.add_scalar('eMI_ema', eMI, i)
+        writer.add_scalar('True_MI', true_mi, i)
+
     plt.figure()
     x = np.arange(total_steps)
-    y_true = analystic_mi * np.ones_like(eMIs)
+    y_true = true_mi * np.ones_like(eMIs)
     plt.plot(x, y_true, 'r-', x, eMIs, 'b-')
     plt.show()
 
